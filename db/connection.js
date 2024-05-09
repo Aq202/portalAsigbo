2669e00a643e19f63d655758bff4e668adb11363
Aq202 <diegoguatedb2002@gmail.com>
2024-05-08 20:12:14 -0600

    Eventos

--- db/connection.js
+++ db/connection.js
@@ -6,12 +6,18 @@ const uri = config.get('dbConnectionUri');
 const connect = () => mongoose.connect(uri);
 
 const { connection } = mongoose;
-
 connection.on('error', () => console.error.bind(console, 'connection error'));
 
 connection.once('open', () => {
   console.info('Conexión a la bd exitosa.');
 });
+
+mongoose.connection.on('connected', () => console.log('connected'));
+mongoose.connection.on('open', () => console.log('open'));
+mongoose.connection.on('disconnected', () => console.log('disconnected'));
+mongoose.connection.on('reconnected', () => console.log('reconnected'));
+mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
+mongoose.connection.on('close', () => console.log('close'));
 
 export default connect;
 export { connection };
